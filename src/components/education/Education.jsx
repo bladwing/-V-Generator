589fa02d1ec3c94746ac67ad1c7ddbd48494ba3b
bladwing <timoti.ponarenko@gmail.com>
2023-02-06 22:35:19 +0400

    create education html input template

--- src/components/education/Education.jsx
+++ src/components/education/Education.jsx
@@ -1,7 +1,53 @@
-import { Fragment } from "react";
+import { Link } from "react-router-dom";
 
 const Education = () => {
-    return ( <Fragment>This is a Education</Fragment> );
-}
- 
+  return (
+    <form action="">
+      <table cellPadding={30}>
+        <thead>
+          <tr>
+            <td>
+              <label htmlFor="">სასწავლებელი</label>
+              <input type="text" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label htmlFor="">ხარისხი</label>
+              <select name="" id="">
+                <option value=""></option>
+              </select>
+            </td>
+
+            <td>
+              <label htmlFor="">დამთავრების რიცხვი</label>
+              <input type="date" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label htmlFor="">აღწერა</label>
+              <textarea type="text" />
+            </td>
+          </tr>
+
+          <tr>
+            <td>
+              <Link to="">სხვა სასწავლებლის დამატება</Link>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <Link to="/experience">ᲣᲙᲐᲜ</Link>
+            </td>
+            <td>
+              <Link to="/resume">ᲓᲐᲡᲠᲣᲚᲔᲑᲐ</Link>
+            </td>
+          </tr>
+        </thead>
+      </table>
+    </form>
+  );
+};
+
 export default Education;
