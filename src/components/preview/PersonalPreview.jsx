f1dadf3a7aa4d05c8549519d0764f6157d62f396
bladwing <timoti.ponarenko@gmail.com>
2023-02-12 22:35:01 +0400

    create style for preview information

--- src/components/preview/PersonalPreview.jsx
+++ src/components/preview/PersonalPreview.jsx
@@ -5,45 +5,32 @@ import "./preview.scss";
 const PersonalPreview = (props) => {
   const [data, setData] = useState("");
 
-  useEffect(() => {setData(props.name)
-  }, []);
+  useEffect(() => {}, []);
 
   console.log(data);
-  console.log(props.name)
+  console.log(props.name);
 
-console.log(props.img)
-
+  console.log(props.img);
 
   return (
-    <div className="SECTION">
-      <table>
-        <thead>
-          <tr>
-            <td>
-              <h1>{props.name}</h1>
-            </td>
-            <td>
-            <h1>{props.lastname}</h1>
-            </td>
-            <td>
-              <img src={props.img} alt="logo" style={{width: "100px", height: "100px", border: "1px solid red", padding: "30px", borderRadus: "30px"}} />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <h3>{props.email}</h3>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <h3>{props.phone}</h3>
-            </td>
-          </tr>
-          <tr>
-            <td>{props.about}</td>
-          </tr>
-        </thead>
-      </table>
+    <div className="section">
+      <div className="personal-preview">
+        <div className="information-container">
+          <h1>
+            {props.name} {props.lastname}
+          </h1>
+
+          <h3>{props.email}</h3>
+          <h3>{props.phone}</h3>
+          <p>{props.about}</p>
+        </div>
+        <img className="profile-photo"
+            src={props.img}
+            alt="logo"
+          />
+            
+      
+      </div>
     </div>
   );
 };
